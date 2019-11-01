dcd1524cdcc9cf11dd5b2e24ab5f609734494a22
Joseph_Tully <tully4school@gmail.com>
2019-10-31 21:17:46 -0400

    having issues with my files this is a test

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "./Form.css";
+import { format } from "path";
 
 const Form = props => {
 	let classList = "";
 	let types = ["input", "select", "textButton", "checkbox"];
 	if (types.includes(props.type)) {
-		if (props.email) {
-			classList += ` form-${props.type}`;
-		}
+		classList += ` form-${props.type}`;
+		console.log(props.type);
+	}
+	if (props.small) {
+		classList += ` form-${props.type}-sm`;
+	}
+	if (props.medium) {
+		classList += ` form-${props.type}-md`;
+	}
+	if (props.large) {
+		classList += ` form-${props.type}-lg`;
 	}
 	return (
-		<div className='form-small'>
+		<div className='form'>
 			<label htmlFor={props.name} className='form-label'>
 				{props.title}
 			</label>
 			<input
-				className={`form-${props.type}`}
 				name={props.email}
 				type={props.type}
 				value={props.value}
--- src/components/Form/Form.stories.js
+++ src/components/Form/Form.stories.js
@@ -2,14 +2,37 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Form from "./Form.js";
 
-storiesOf("Form", module).add("Small Form", () => (
-	<Form
-		title='Email'
-		email='Email'
-		type='input'
-		value=''
-		handleChange=''
-		placeholder='Email'
-		small
-	/>
-));
+storiesOf("Form", module)
+	.add("Small Form", () => (
+		<Form
+			title='Email'
+			email='Email'
+			type='input'
+			value=''
+			handleChange=''
+			placeholder='Email'
+			small
+		/>
+	))
+	.add("Medium Form", () => (
+		<Form
+			title='Email'
+			email='Email'
+			type='input'
+			value=''
+			handleChange=''
+			placeholder='Email'
+			medium
+		/>
+	))
+	.add("Large Form", () => (
+		<Form
+			title='Email'
+			email='Email'
+			type='input'
+			value=''
+			handleChange=''
+			placeholder='Email'
+			large
+		/>
+	));
