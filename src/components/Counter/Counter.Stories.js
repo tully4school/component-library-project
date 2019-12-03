daf765e1fa7204b56e2a817a77f5b66b43528867
Joseph_Tully <tully4school@gmail.com>
2019-12-03 12:17:42 -0500

    updated counter

--- src/components/Counter/Counter.Stories.js
+++ src/components/Counter/Counter.Stories.js
@@ -2,6 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Counter from "./Counter.js";
 let increaseCount;
-storiesOf("Counter", module).add("Counter", () => (
+storiesOf("Counter", module).add("Counter Component", () => (
 	<Counter className='counter-button' value={0} min={-10} max={11} />
 ));
